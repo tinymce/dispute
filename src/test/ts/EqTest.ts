fe0a70c3cc0bb8b64dbd692a5c78fe131f3f1a53
Dylan Just <dylan.just@ephox.com>
2019-09-23 12:39:20 +1000

    Property tests

--- src/test/ts/EqTest.ts
+++ src/test/ts/EqTest.ts
@@ -1,5 +1,6 @@
 import { eqAny, eqNull, eqUndefined, eqNumber, eqString, eqArray, eqRecord, contramap } from '../../main/ts/api/Eq';
 import { assert } from 'chai'
+import * as fc from 'fast-check';
 
 describe('eqNull', () => {
   it('eqs', () => {
@@ -19,13 +20,15 @@ describe('eqUndefined', () => {
 
 describe('eqNumber', () => {
   it('eqs', () => {
-    assert.strictEqual(eqNumber.eq(3, 3), true);
-    assert.strictEqual(eqNumber.eq(3, 7), false);
+    fc.assert(fc.property(fc.integer(), (n) => eqNumber.eq(n, n)));
+    fc.assert(fc.property(fc.integer(), (n) => !eqNumber.eq(n, n + 1)));
   });
 });
 
 describe('eqString', () => {
   it('eqs', () => {
+    fc.assert(fc.property(fc.string(), (s) => eqString.eq(s, s)));
+    fc.assert(fc.property(fc.string(), fc.char(), (s, c) => !eqString.eq(s, s + c)));
     assert.strictEqual(eqString.eq('cat', 'cat'), true);
     assert.strictEqual(eqString.eq('', ''), true);
     assert.strictEqual(eqString.eq('', 'z'), false);
@@ -34,6 +37,8 @@ describe('eqString', () => {
 
 describe('eqArrayNumber', () => {
   it('eqs', () => {
+    fc.assert(fc.property(fc.array(fc.integer()), (xs) => eqArray(eqNumber).eq(xs, xs)));
+    fc.assert(fc.property(fc.array(fc.integer()), (xs) => eqArray(eqNumber).eq(xs, Array.prototype.slice.call(xs))));
     assert.strictEqual(eqArray(eqNumber).eq([], []), true);
     assert.strictEqual(eqArray(eqNumber).eq([1], [1]), true);
     assert.strictEqual(eqArray(eqNumber).eq([0], [1]), false);
@@ -42,6 +47,8 @@ describe('eqArrayNumber', () => {
 
 describe('eqArrayArray', () => {
   it('eqs', () => {
+    fc.assert(fc.property(fc.array(fc.array(fc.string())), (xss) => eqArray(eqArray(eqString)).eq(xss, xss)));
+    fc.assert(fc.property(fc.array(fc.array(fc.string())), (xss) => eqArray(eqArray(eqString)).eq(xss, Array.prototype.slice.call(xss))));
     assert.strictEqual(eqArray(eqArray(eqString)).eq([], []), true);
     assert.strictEqual(eqArray(eqArray(eqString)).eq([[]], [[]]), true);
     assert.strictEqual(eqArray(eqArray(eqString)).eq([[], []], [[], []]), true);
@@ -64,6 +71,9 @@ describe('eqRecord', () => {
 
 describe('eqAny', () => {
   it('eqs', () => {
+    fc.assert(fc.property(fc.anything(), (x) => isNaN(x) || eqAny.eq(x, x)));
+    fc.assert(fc.property(fc.anything(), (x) => isNaN(x) || !eqAny.eq(x, {z: x})));
+
     assert.strictEqual(eqAny.eq(1, 1), true);
     assert.strictEqual(eqAny.eq(1, 2), false);
     assert.strictEqual(eqAny.eq(1, null), false);
@@ -101,6 +111,10 @@ describe('contramap', () => {
       x: number;
     }
     const eqNumbo = contramap<number, Numbo>(eqNumber, (n) => n.x);
+
+    const arb = fc.integer().map((x) => ({x}));
+
+    fc.assert(fc.property(arb, (x) => eqNumbo.eq(x, ({x: x.x}))));
 
     assert.strictEqual(eqNumbo.eq({x: 3}, {x: 3}), true);
     assert.strictEqual(eqNumbo.eq({x: 3}, {x: 4}), false);
